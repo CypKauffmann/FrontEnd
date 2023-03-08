adf6aea5262377c22f799eed7a526aacd0bfd442
CypKauffmann <71978243+CypKauffmann@users.noreply.github.com>
2023-03-08 12:18:20 +0100

    Creations des classes services personnes et utilisateurs + routing

---
+++ src/app/services/personne.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Personne } from '../models/personne';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class personneService {
+  constructor(private http: HttpClient) {}
+
+  getAll() {
+    return this.http.get<Personne[]>('http://localhost:8015/api/personnes');
+  }
+
+  getById(id: number) {
+    return this.http.get<Personne>(`http://localhost:8015/api/personnes/${id}`);
+  }
+
+  getByHist(id: number) {
+    return this.http.get<Personne>(
+      `http://localhost:8015/api/personnesByHist/${id}`
+    );
+  }
+
+  getByRdv(id: number) {
+    return this.http.get<Personne>(
+      `http://localhost:8015/api/personnesByRdv/${id}`
+    );
+  }
+
+  add(personne: Personne) {
+    return this.http.post('http://localhost:8015/api/personnes', personne);
+  }
+
+  delete(id: number) {
+    return this.http.delete(`http://localhost:8015/api/personnes/${id}`);
+  }
+}
