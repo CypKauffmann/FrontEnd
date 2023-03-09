afd3b7fd3b4d8c13d93d0337b06c121251f28baf
dupin_jade <jadedupin00@gmail.com>
2023-03-09 17:25:22 +0100

    modif front espace commercial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-<<<<<<< HEAD
-import { CommercialComponent } from './composant/commercial/commercial.component';
-import { RendezvousComponent } from './composant/rendezvous/rendezvous.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-=======
 import { GestionUtilisateurComponent } from './composant/gestion-utilisateur/gestion-utilisateur.component';
 import { GestionFormateurComponent } from './composant/gestion-formateur/gestion-formateur.component';
 import { GestionFormationComponent } from './composant/gestion-formation/gestion-formation.component';
->>>>>>> main
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CommercialComponent } from './composant/commercial/commercial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     CommercialComponent,
-    RendezvousComponent
-=======
     GestionUtilisateurComponent,
     GestionFormateurComponent,
     GestionFormationComponent
->>>>>>> main
   ],
   imports: [
     BrowserModule,
--- src/app/composant/commercial/commercial.component.ts
+++ src/app/composant/commercial/commercial.component.ts
@@ -1,35 +1,129 @@
+import { DatePipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Personne } from 'src/app/models/personne';
 import { RendezVous } from 'src/app/models/rendez-vous';
+import { Utilisateur } from 'src/app/models/utilisateur';
+import { personneService } from 'src/app/services/personne.service';
 import { RendezvousService } from 'src/app/services/rendezvous.service';
+import { utilisateurService } from 'src/app/services/utilisateur.service';
 
 @Component({
   selector: 'app-commercial',
   templateUrl: './commercial.component.html',
-  styleUrls: ['./commercial.component.css']
+  styleUrls: ['./commercial.component.css'],
+  providers: [DatePipe]
 })
 export class CommercialComponent implements OnInit {
 
-  constructor(private rdvservice:RendezvousService,
+  constructor(private rdvservice:RendezvousService, private persservice:personneService,
+    private utservice:utilisateurService,
     private router:Router, private route:ActivatedRoute)
   {
 
   }
 
+
   rendezvous!:RendezVous[] ;
   rdv!:RendezVous ;
+
+  prospects!:Personne[] ;
+  prospect!:Personne ;
+  idProspect!:number ;
+
+  personnes!:Personne[] ;
+
+  utilisateurs!:Utilisateur[] ;
 
 
   ngOnInit(): void {
 
-    this.rdv = new RendezVous ;
+    //tous les rdv
+    this.afficherAllRdv() ;
 
+    this.rdv = new RendezVous ;
+    
+    //liste prospects
+    this.persservice.getAll().subscribe(
+      response=>  
+      {
+        this.personnes=response
+
+        this.utservice.getAll().subscribe(
+          response => 
+          {
+            this.utilisateurs = response
+
+            this.prospects = this.personnes?.filter((personne) => {
+              return !this.utilisateurs.some((p) => p.idPers === personne.idPers)
+            });
+          }
+        )
+      }
+    );
+
+    // this.persservice.getById(this.idProspect).subscribe(
+    //   response=> this.prospect=response
+    // );
+    
+  }
+
+
+
+  afficherAllRdv()
+  {
     this.rdvservice.getAll().subscribe(
-      response => this.rendezvous=response
+      response => 
+      {
+        this.rendezvous = this.sortObjectsByDateTime(response)
+      }
+    );
+  }
+
+  sortObjectsByDateTime(list: RendezVous[]): RendezVous[] {
+    return list.sort((a, b) => {
+      const dateA = Date.parse(a.dateRdv as unknown as string);
+      const dateB = Date.parse(b.dateRdv as unknown as string);
+      return dateA - dateB;
+    });
+  }
+
+  ajoutRdv()
+  {
+    this.persservice.getById(this.idProspect).subscribe(
+      response=>
+      {
+        this.prospect=response
+        this.rdv.personne = this.prospect
+
+        this.rdvservice.add(this.rdv).subscribe(
+          response=> this.afficherAllRdv()
+        )
+        this.rdv = new RendezVous
+      }
     );
     
   }
 
+
+  supprimerRdv(id:number)
+  {
+    this.rdvservice.delete(id).subscribe(
+      response=> this.afficherAllRdv()
+    );
+  }
+
+
+  modifierRdv(id:number)
+  {
+    this.rdvservice.getById(id).subscribe(
+      response=> 
+      {
+        this.rdv=response
+      }
+    );
+  }
 
 
 }
